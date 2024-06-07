71f1f8e330120b8af224ba037186dedabfd9c194
MuriloMoreir <124629928+MuriloMoreir@users.noreply.github.com>
2024-06-07 07:29:31 -0300

    Update rpa_teste.py

--- rpa_teste.py
+++ rpa_teste.py
@@ -32,11 +32,11 @@ print('Dólar: ', dolar_site)
 
 
 # Conectando com o DB
-conexao = psycopg2.connect(database = "dbcotacaodolar",
-                           host = "localhost",
-                           user = "postgres",
-                           password = "1234",
-                           port = "5432")
+conexao = psycopg2.connect(database = "dbCotacao",
+                           host = "pg-3f7b996d-muriloolimora971.f.aivencloud.com",
+                           user = "avnadmin",
+                           password = "AVNS_BjsAizQig1olY9q0atk",
+                           port = "23734")
 
 print(conexao.info)
 print(conexao.status)
